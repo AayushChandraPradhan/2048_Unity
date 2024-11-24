c3b7d4e5a2432613dae92d9b960a1b9a7489a92f
Aayush <laptop@laptop.com>
2024-11-24 20:08:07 +0545

    upload

---
+++ Assets/Script/TileScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tile State")]
+public class TileScript : ScriptableObject
+{
+    public Color backgroundColor;
+    public Color textColor;
+
+
+
+
+
+} // end
